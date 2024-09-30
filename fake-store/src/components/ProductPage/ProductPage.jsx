60b8e61e196a4f31c8d58e2b2fbb870fa29db744
DmitriiHolubtsov <dmitriiholubtsov@gmail.com>
2024-09-30 23:56:21 +0300

    Add fixes

--- fake-store/src/components/ProductPage/ProductPage.jsx
+++ fake-store/src/components/ProductPage/ProductPage.jsx
@@ -5,6 +5,7 @@ import { useGetProductByIdQuery } from "../../redux/productApiSlice";
 import { addItem } from "../../redux/cartSlice";
 import { Button, CircularProgress } from "@mui/material";
 import Header from "../Header/Header";
+import styles from "./ProductPage.module.scss";
 
 const ProductPage = () => {
   const { id } = useParams();
@@ -30,22 +31,24 @@ const ProductPage = () => {
   return (
     <>
       <Header />
-      <div>
-        <h1>{product.title}</h1>
+      <div className={styles.productContainer}>
         <img
           src={product.image}
           alt={product.title}
           style={{ width: "300px" }}
         />
-        <p>{product.description}</p>
-        <p>${product.price}</p>
-        <input
-          type="number"
-          value={quantity}
-          onChange={(e) => setQuantity(Number(e.target.value))}
-          min="1"
-        />
-        <Button onClick={handleAddToCart}>Add to Cart</Button>
+        <div className={styles.productInfo}>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <p>${product.price}</p>
+          <input
+            type="number"
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            min="1"
+          />
+          <Button onClick={handleAddToCart}>Add to Cart</Button>
+        </div>
       </div>
     </>
   );
